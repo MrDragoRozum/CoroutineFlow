af6542a667930b8b725bfe37af81cfa2f40e9867
Alexander Rozum <roozumaleks@gmail.com>
2023-10-28 23:13:29 +0300

    Рефакторинг crypto app

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoRepository.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoRepository.kt
@@ -1,6 +1,8 @@
 package com.sumin.coroutineflow.crypto_app
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlin.random.Random
 
 object CryptoRepository {
@@ -8,12 +10,20 @@ object CryptoRepository {
     private val currencyNames = listOf("BTC", "ETH", "USDT", "BNB", "USDC")
     private val currencyList = mutableListOf<Currency>()
 
-    suspend fun getCurrencyList(): List<Currency> {
-        delay(3000)
-        generateCurrencyList()
-        return currencyList.toList()
-    }
+//    suspend fun getCurrencyList(): List<Currency> {
+//        delay(3000)
+//        generateCurrencyList()
+//        return currencyList.toList()
+//    }
 
+    fun getCurrencyList(): Flow<List<Currency>> = flow {
+        while(true) {
+            generateCurrencyList()
+            delay(3000)
+            emit(currencyList.toList())
+            delay(3000)
+        }
+    }
     private fun generateCurrencyList() {
         val prices = buildList {
             repeat(currencyNames.size) {
