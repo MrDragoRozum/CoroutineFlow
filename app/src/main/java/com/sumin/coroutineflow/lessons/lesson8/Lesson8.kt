151344bca09b1086905ba68bb34da1486e265cb8
Alexander Rozum <roozumaleks@gmail.com>
2023-10-31 13:31:01 +0300

    Холодный Flow

---
+++ app/src/main/java/com/sumin/coroutineflow/lessons/lesson8/Lesson8.kt
@@ -0,0 +1,33 @@
+package com.sumin.coroutineflow.lessons.lesson8
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+suspend fun main() {
+    val job1 = coroutineScope.launch {
+        getFlow().collect {
+            println(it)
+        }
+    }
+    val job2 = coroutineScope.launch {
+        getFlow().collect {
+            println(it)
+        }
+    }
+    job1.join()
+    job2.join()
+}
+
+fun getFlow(): Flow<Int> = flow {
+    repeat(10) {
+        println("Emitted: $it")
+        emit(it)
+        delay(1000)
+    }
+}
