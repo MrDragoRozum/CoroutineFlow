76f98733cc4405b1e6ffed8d17dafa6cfb639982
Alexander Rozum <roozumaleks@gmail.com>
2023-11-02 18:23:50 +0300

    Использован StateFlow вместо LiveData

--- app/src/main/java/com/sumin/coroutineflow/team_score/TeamScoreViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/team_score/TeamScoreViewModel.kt
@@ -1,38 +1,46 @@
 package com.sumin.coroutineflow.team_score
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class TeamScoreViewModel : ViewModel() {
 
-    private val _state = MutableLiveData<TeamScoreState>(TeamScoreState.Game(0, 0))
-    val state: LiveData<TeamScoreState> = _state
+    private val _stateFlow = MutableStateFlow<TeamScoreState>(TeamScoreState.Game(0, 0))
+    val stateFlow = _stateFlow.asStateFlow()
 
     fun increaseScore(team: Team) {
-        val currentState = _state.value
+        val currentState = _stateFlow.value
         if (currentState is TeamScoreState.Game) {
             if (team == Team.TEAM_1) {
                 val oldValue = currentState.score1
                 val newValue = oldValue + 1
-                _state.value = currentState.copy(score1 = newValue)
+
+                _stateFlow.value = currentState.copy(score1 = newValue)
+
                 if (newValue >= WINNER_SCORE) {
-                    _state.value = TeamScoreState.Winner(
+
+                    _stateFlow.value = TeamScoreState.Winner(
                         winnerTeam = Team.TEAM_1,
                         newValue,
                         currentState.score2
+
                     )
                 }
             } else {
                 val oldValue = currentState.score2
                 val newValue = oldValue + 1
-                _state.value = currentState.copy(score2 = newValue)
+
+                _stateFlow.value = currentState.copy(score2 = newValue)
+
                 if (newValue >= WINNER_SCORE) {
-                    _state.value = TeamScoreState.Winner(
+
+                    _stateFlow.value = TeamScoreState.Winner(
                         winnerTeam = Team.TEAM_2,
                         currentState.score1,
                         newValue
                     )
+
                 }
             }
         }
